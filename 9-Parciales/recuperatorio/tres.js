b7eb9c69c7b090ff01c49c449164d0b7f5dcbfab
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-06 20:57:02 -0300

    Add files via upload

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,22 @@
+
 function Mostrar()
 {
+	var precio1, precio2, precio3;
+	var resultado;
+	var promedio;
+
+		precio1=document.getElementById('precioUno').value;
+		precio2=document.getElementById('precioDos').value;
+		precio3=document.getElementById('precioTres').value;
+
+		precio1=parseInt(precio1);
+		precio2=parseInt(precio2);
+		precio3=parseInt(precio3);
+
+		resultado=precio1+precio2+precio3;
+		promedio=resultado/3;
+
+		alert("La suma de todos los precios es "+ resultado+
+			  "\nEl promedio es "+promedio);		
 
 }
