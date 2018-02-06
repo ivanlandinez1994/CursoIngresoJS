7310cabbc7d8e3a7bf09ba0da419ac55a3aeef26
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-06 18:40:47 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,62 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+    var largo, ancho;
+    var cantidadAlambre;
+    
+        largo=document.getElementById('Largo').value;
+        ancho=document.getElementById('Ancho').value;
+        
+        largo=parseInt(largo);
+        ancho=parseInt(ancho);
+        cantidadAlambre=parseInt(cantidadAlambre);
+        
+        cantidadAlambre=((largo+ancho)*2)*3;
+        
+        alert("la cantidad de alambre a comprar es de "+ cantidadAlambre);
 }
 function Circulo () 
 {
-	
+    var radio;
+    var area_terreno;
+    var cantidadAlambre;
+        
+        radio=document.getElementById('Radio').value;
+        
+        radio=parseInt(radio);
+        cantidadAlambre=parseInt(cantidadAlambre);
+        area_terreno=parseInt(area_terreno);
+        
+        area_terreno=radio*radio;
+        cantidadAlambre=area_terreno*3;
+        
+        alert("dado que el area del terreno es de "+area_terreno+ 
+        "\nla cantidad de alambre que se debe comprar es "+cantidadAlambre);
+        /* la opcion \n sirve para dar un espacio en el texto que aparecera en pantalla 
+        como si fuera un enter.*/
 }
 function Materiales () 
 {
-	
+    var largo, ancho;
+    var bolsasCemento, bolsasKal;
+    var cemento=2 , kal=3 ;
+    
+        
+        largo=document.getElementById('Largo').value;
+        ancho=document.getElementById('Ancho').value;
+        
+        largo=parseInt(largo);
+        ancho=parseInt(ancho);
+        bolsasCemento=parseInt(bolsasCemento);
+        bolsasKal=parseInt(bolsasKal);
+        
+        bolsasCemento= cemento*(largo*ancho);
+        bolsasKal= kal*(largo*ancho);
+        
+        alert("para las medidas del terreno ingresado necesitara:\n"+bolsasCemento+" bolsas de cemento.\n"+
+              bolsasKal+" bolsas de kal.");
+        
+        
+        
+        
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,60 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precio_p1, precio_p2, precio_p3;
+    var resultado;
+    
+        precio_p1=document.getElementById('PrecioUno').value;
+        precio_p2=document.getElementById('PrecioDos').value;
+        precio_p3=document.getElementById('PrecioTres').value;
+        
+        precio_p1=parseInt(precio_p1);
+        precio_p2=parseInt(precio_p2);
+        precio_p3=parseInt(precio_p3);
+        resultado=parseInt(resultado);
+        
+        resultado=precio_p1+precio_p2+precio_p3;
+        
+        alert("el precio final de los productos es de "+resultado);
+        
 }
 function Promedio () 
 {
-	
+    var precio_p1, precio_p2, precio_p3;
+    var resultado;
+    
+        precio_p1=document.getElementById('PrecioUno').value;
+        precio_p3=document.getElementById('PrecioDos').value;
+        precio_p2=document.getElementById('PrecioTres').value;
+        
+        precio_p1=parseInt(precio_p1);
+        precio_p2=parseInt(precio_p2);
+        precio_p3=parseInt(precio_p3);
+        resultado=parseInt(resultado);
+        
+        resultado=(precio_p1+precio_p2+precio_p3)/3;
+        
+        alert("el promedio del precio de los productos es "+ resultado);
 }
 function PrecioFinal () 
 {
-	
+    var precio_p1, precio_p2, precio_p3;
+    var resultado;
+    var iva;
+    
+        precio_p1=document.getElementById('PrecioUno').value;
+        precio_p2=document.getElementById('PrecioDos').value;
+        precio_p3=document.getElementById('PrecioTres').value;
+        
+        precio_p1=parseInt(precio_p1);
+        precio_p2=parseInt(precio_p2);
+        precio_p3=parseInt(precio_p3);        
+        resultado=parseInt(resultado);
+        iva=parseInt(iva);
+        
+        iva=((precio_p1+precio_p2+precio_p3)*0.21);
+        resultado= (precio_p1+precio_p2+precio_p3) + iva;
+        
+        alert("el precio final de los productos mas el iva es de "+resultado);
+        
 }
