69871450212c922e46eacbe09a34749e94fbbc26
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-06 18:39:47 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+    var importe;
+    var importe_menos_descuento;
+    
+        importe=document.getElementById('importe').value;        
+        
+        importe=parseInt(importe);
+        importe_menos_descuento=parseInt(importe_menos_descuento);
+        
+        importe_menos_descuento=importe-(importe*0.25);
+        document.getElementById('resultado').value=importe_menos_descuento;
+        
+        
 }
