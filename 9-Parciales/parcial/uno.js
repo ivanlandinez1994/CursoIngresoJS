04fb3ec4110ab7071fd7cf3734f4583504495216
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-06 20:57:34 -0300

    Add files via upload

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+	var base;
+	var perimetro;
 
+		base=document.getElementById('laBase').value;
+
+		perimetro=base*base;
+
+		alert("el perimetro del cuadrado es "+perimetro);
 }
