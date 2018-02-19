9cf99cc297feecd35509836e77d87e22f6ac38f5
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-19 20:32:43 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,27 +1,19 @@
 function Mostrar()
 {
-var  numero=[5]; 
-// por que al crear el array desde el inicio queda con el mismo valor en todos los espacios, esto depende del numero 
-// del array por ejemplo este array es de tamaño 5 y al inicio cada espacio del array queda marcado con 5.    
-    var contador=0;    
-    
-    while(contador<5){
-        numero[contador]=prompt("ingrese el numero "+(contador+1)); 
-        numero[contador]=parseInt(numero[contador]);
-        contador++;        
-    }
-    
-    var resultadoSuma=0;
-    for(var i=0; i<5; i++){                
-        resultadoSuma=parseInt(resultadoSuma);
-        resultadoSuma=resultadoSuma+numero[i];           
-    }      
-    
-    var elPromedio;
-        elPromedio=resultadoSuma/5;        
-        
-        document.getElementById('suma').value=resultadoSuma;
-        document.getElementById('promedio').value=elPromedio;
-    
-    
+    var numeros;
+    var sumaAcumulada=0;
+    var contador=0;
+    var promedio;
+        while(contador<5){
+            numeros=prompt("ingrese el nunero "+(contador+1));
+            sumaAcumulada=parseInt(sumaAcumulada);
+            numeros=parseInt(numeros);
+            sumaAcumulada=sumaAcumulada+numeros;
+            contador++;
+
+        }
+        promedio=sumaAcumulada/5;
+
+        document.getElementById('suma').value=sumaAcumulada;
+        document.getElementById('promedio').value=promedio;
 }//FIN DE LA FUNCIÃ“N
