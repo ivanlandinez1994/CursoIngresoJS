74df040fe8f351d4173a823ae4fd89f1c5a3e190
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-19 21:41:36 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,7 +3,7 @@ function Mostrar()
     var numero;
     var parar;
     var contador=0; 
-    var max=0, min=0;           
+    var max, min;           
         
         while(parar!="parar"){
             numero=prompt("ingrese numero "+ (contador+1));
@@ -11,15 +11,21 @@ function Mostrar()
             parar=prompt("si desea parar de ingresar numeros escriba parar");
             parar=parar.toUpperCase();
             parar=parar.toLowerCase();
-            if(numero>max){
+
+            if(contador==1){
+                min=numero;
                 max=numero;
             }
-            if(min==0){
-                min=numero;
-            }
-            if(numero<min){
-                min=numero;
-            }
+
+            else{
+                if(numero>max){
+                    max=numero;
+                }
+
+                if(numero<min){
+                    min=numero;
+                }
+            }   
 
         }
                 
