cde6dc7e821ddafc52d9eed9df769403525fa2ba
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-19 21:37:03 -0300

    Add files via upload

--- 9-Parciales/recuperatorio2/2-entradaSalida.js
+++ 9-Parciales/recuperatorio2/2-entradaSalida.js
@@ -1,7 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+	var importe;
+	var precioMasIva;
+		importe=prompt("Ingrese el importe del producto");
+
+		precioMasIva=importe*1.21;
+
+		document.getElementById('importe').value=precioMasIva;
 	
 }
 
