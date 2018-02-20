132a43667e14c91e895f6b543c4a2b8720d8aaee
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-19 21:50:50 -0300

    Add files via upload

--- 9-Parciales/recuperatorio2/3-entradaSalida.js
+++ 9-Parciales/recuperatorio2/3-entradaSalida.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var anchoTerreno;
+	var largoTerreno;
+	var perimetro;
+	var cantidadAlambre;
+
+		anchoTerreno=document.getElementById('ancho').value;
+		anchoTerreno=parseInt(anchoTerreno);
+		largoTerreno=document.getElementById('largo').value;
+		largoTerreno=parseInt(largoTerreno);
+
+		perimetro=(anchoTerreno+largoTerreno)*2;
+		cantidadAlambre=perimetro*6;
+
+		alert("se necesitan " +cantidadAlambre+" metros de alambre");
+
 	
 }
 
