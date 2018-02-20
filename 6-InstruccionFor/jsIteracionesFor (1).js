1e10390b8a107d7642afb76ffa8bd4bd5dcd15a0
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-20 20:03:07 -0300

    continue y break

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
-    for(var numero=1;numero<11;numero++){
+    /*for(var numero=1;numero<11;numero++){
         alert(numero);
+    }*/
+    var contador=0;
+    for(;;){
+    	contador++;
+    	document.write("<br>"+contador);
+    	if(contador==10){
+    		document.write("<br>break"+contador);
+    		break;
+    	}
+    	if(contador==5){    		
+    		continue;
+    	}
+    	document.write("<br>continue "+contador);
     }
 }
