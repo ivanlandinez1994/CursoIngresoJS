3f6e597eef30e401e1d91528a999b902c9df5dbe
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-20 22:10:56 -0300

    Update jsIteracionesFor (7).js

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,12 +1,22 @@
 function Mostrar()
 {
-    var numero;
-    var divisores;
+    var numero;    
+    var anterior
+    var contadorDivisores;
     
         numero=prompt("ingrese un numero");
-        
-        for(var i=0; i<numero; i++){
-            alert();
+        numero=parseInt(numero);
+        for(anterior=2; anterior<numero; anterior++){
+            if(numero%anterior==0){
+                document.write("<br>"+anterior);
+                contadorDivisores++;
+            }
+            if(anterior>(numero/2)){
+                break;
+            }
+        }
+        if(contadorDivisores==0){
+            document.write("es primo el numero ingresado");    
         }
 
 //no dice por cual quieren que sepamos si es divisible;
