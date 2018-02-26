1524b6e34b7d5124b4901c30f8043c3daa055262
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-26 19:31:41 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,14 +1,23 @@
 function Mostrar()
 {
     var importe;
+    var descuento;
+    var descuentoaRealizar;
     var importeFInal;
     
     importe=prompt("ingrese el importe del producto");
+    descuento=prompt("Ingrese el porcentaje de descuento a realizar");
+
+    descuentoaRealizar=descuento/100;
     
-    importeFinal=" el importe final con IVA (21%) es: "+(importe*1.21)+"$";
+    importeFinal=" el importe final es "+(importe-(importe*descuentoaRealizar));
     
     document.getElementById('importeFinal').value=importeFinal;
     
     
     
 }
+/*
+	pedir precio, y porcentaje de descuento que se va a hacer 
+	mostrar precio final
+*/
