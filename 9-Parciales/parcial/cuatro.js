65d7d29838d0669d7cf85ac4496dcac380d414c5
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-26 20:37:43 -0300

    Add files via upload

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,23 +1,56 @@
 function Mostrar()
 {
-    var numeroUno, numeroDos;
-    var resultado;
+    var numeroUno, numeroDos, numeroTres;
+    var numeroMayor;
+    var numeroMenor;
     
     numeroUno=prompt("Ingrese el numero 1");
+    numeroUno=parseInt(numeroUno);
     numeroDos=prompt("Ingrese el numero 2");
+    numeroDos=parseInt(numeroDos);
+    numeroTres=prompt("ingrese el numero 3");
+    numeroTres=parseInt(numeroTres);
     
-    if(numeroUno==numeroDos){
-        resultado=numeroUno*numeroDos;        
-        document.write("Dado que los numeros son iguales se procede a multiplicar y el resultado es "+resultado);
+    if(numeroUno>numeroDos && numeroUno>numeroTres){
+        numeroMayor=numeroUno;
+        if(numeroDos<numeroTres){
+            numeroMenor=numeroDos;
+        }
+        else{
+            numeroMenor=numeroTres;
+        }
     }
-    else if(numeroUno>numeroDos){
-        resultado=numeroUno-numeroDos;
-        document.write("Dado que el primer numero es mayor al segundo se procede a restar y el resultado es "+resultado);
+    else if(numeroDos>numeroUno && numeroDos>numeroTres){
+        numeroMayor=numeroDos 
+        if(numeroUno<numeroTres)       {
+            numeroMenor=numeroUno;
+        }
+        else{
+            numeroMenor=numeroTres;
+        }
     }
     else {
-        numeroUno=parseInt(numeroUno);
-        numeroDos=parseInt(numeroDos);
-        resultado=numeroUno+numeroDos;
-        document.write("Dado que el segundo numero es mayor que el primero se procede a sumar y el resultado es "+resultado);
+        numeroMayor=numeroTres;
+        if(numeroUno<numeroDos){
+            numeroMenor=numeroUno;
+        }
+        else{
+            numeroMenor=numeroDos;
+        }
     }
+
+    /*if(numeroUno<numeroDos && numeroUno<numeroTres){
+        numeroMenor=numeroUno;
+    }
+    else if(numeroDos<numeroUno && numeroDos<numeroTres){
+        numeroMenor=numeroDos        
+    }
+    else {
+        numeroMenor=numeroTres;
+    */
+    document.write("el numero mayor es "+numeroMayor+"<br>el numero menor es "+numeroMenor);
+
 }
+/*
+    ingresar 3 numeros informar cual es el mayor
+*/
