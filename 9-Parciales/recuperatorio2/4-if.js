04d010982f00036230349d8650ad22a1a3f0d429
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-26 19:49:48 -0300

    Add files via upload

--- 9-Parciales/recuperatorio2/4-if.js
+++ 9-Parciales/recuperatorio2/4-if.js
@@ -1,7 +1,24 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+	var numeroUno, numeroDos;
+	var resultado;
+		numeroUno=prompt("Ingrese el numero uno");
+		numeroUno=parseInt(numeroUno);
+		numeroDos=prompt("Ingrese el numero dos");
+		numeroDos=parseInt(numeroDos);
+
+		if(numeroUno==numeroDos){
+			resultado=numeroUno*numeroDos;
+			document.write("dado que son iguales se multiplican y el resultado es "+resultado);
+		}
+		else if(numeroUno>numeroDos){
+			resultado=numeroUno-numeroDos;
+			document.write("dado que el primero es mayor que el segundo se restan y el resultado es "+resultado);	
+		}
+		else{
+			resultado=numeroUno+numeroDos;		
+			document.write("dado que el segundo es mayor que el primero se suman y el resultado es "+resultado)
+		}
 }
 
