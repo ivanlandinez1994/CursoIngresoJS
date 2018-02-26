36b013e95cc8c97418c1fe0ab16b2cc2495f39e6
ivanlandinez1994 <36174762+ivanlandinez1994@users.noreply.github.com>
2018-02-26 19:16:05 -0300

    Add files via upload

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -2,13 +2,18 @@
 function Mostrar()
 {
     var base;
+    var altura;
     var perimetro;
+    var superficie;
     
         base=document.getElementById('laBase').value;
+        altura=prompt("Ingresa la altura del triangulo");
         
-        perimetro=base*base;
+        superficie=(base*altura)/2;
+        perimetro=base*3;
         
-        alert("el perimetro es "+perimetro);
+        document.write("El perimetro es "+perimetro+"<br>La superficie es de "+superficie );
+
         
         
         
